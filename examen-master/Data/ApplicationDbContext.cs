47a0913c1bd306bcd8ea69640e34ba056ad9321f
OscarPineda1 <167844008+OscarPineda1@users.noreply.github.com>
2024-09-29 07:07:19 -0500

    Primer avance pc2

---
+++ examen-master/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace examen.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<examen.Models.Contacto> DataContacto {get; set; }
+    public DbSet<examen.Models.Cuenta> DataCuenta {get; set; }
+}
